d32e4be5a8bcedd72bbb570d33b14ccc3ff1607d
albin-ux <62427356+albin-ux@users.noreply.github.com>
2021-05-12 15:40:35 +0200

    senaste

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
+function getStars(grade){
+  const star = []
+  for(let i=0; i<grade; i++){
+
+    star.push(<img src="./star.png" />)
+  }
+  return star
+}
 
 export default function Movie(props){
   return(
 
     <li className ="list-group-item">
      {props.item.title}
+     {getStars(props.item.grade)}
      <button className="btn btn-sm btn-danger float-end" onClick={() => {props.deleteMovie(props.item.id)}}>X</button>
     </li>
 
--- src/NewMovie.js
+++ src/NewMovie.js
@@ -1,38 +1,60 @@
 import React, { useState, useRef} from 'react';
 import Movie from './Movie';
 export default function NewMovie() {
-  const [movies, setMovie] = useState([{
-    id: 1,
-    title: "first movie"
-
-  },{
-    id: 2,
-    title: "second movie"
-
-  }]);
+  const [movies, setMovie] = useState([]);
   const inputRef = useRef();
+  const anotherRef = useRef();
 
   function AddItem(event){
-    if (event.keyCode === 13){
+    if (inputRef.current.value == ""){
+      alert("Du måste skriva in en titel")
+    }
+    else if (anotherRef.current.value == 0) {
+      alert("Du måste skriva in ett betyg")
+    }
+    else {
       const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
 
       setMovie([...movies, {
         id: newId,
-        title: inputRef.current.value
+        title: inputRef.current.value,
+        grade: anotherRef.current.value
       }]);
 
       inputRef.current.value = "";
+      anotherRef.current.value = "";
     }
-  }
+
+    }
+
   function deleteMovie(id) {
     setMovie(movies.filter((item) => item.id !== id));
   }
+
+  function sortTitle(){
+
+  }
+
+  function sortGrade(){
+    
+  }
   return (
     <div>
-        <input className="form-control" placeholder="Lägg till en ny film" ref={inputRef} onKeyUp={AddItem}/>
+        <input className="form-control" placeholder="Lägg till en ny film" ref={inputRef}/>
+        <select class="form-control" placeholder="Välj ett betyg" ref={anotherRef}>
+                    <option value="0">Välj betyg här...</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+        <button type="submit" className="btn btn-success mt-3" onClick={() => {AddItem()}}>Lägg till film</button>
         <ul className="list-group">
             { movies.map(movie => <Movie key={movie.id} item ={movie} deleteMovie={deleteMovie} />)}
         </ul>
+        <button type="submit" className="btn btn-success mt-3" onClick={() => {AddItem()}}>Lägg till film</button>
+        <button type="submit" className="btn btn-success mt-3" onClick={() => {AddItem()}}>Lägg till film</button>
     </div>
 
   );
